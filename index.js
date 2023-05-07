cc82dc824a7a4ca812c459ac06d65207362c0374
vuslatsena <vuslatsena23@gmail.com>
2023-05-07 11:21:02 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Vuslat",
+  streetAdress: "Serbia"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return { ...obj, [key]: value };
+}
+
+updateEmployeeWithKeyAndValue(employee, "Sena", "Sarajevo");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+
+}
+
+function deleteFromEmployeeByKey(object, key) {
+  let newObj = { ...object }
+  delete newObj[key]
+  return newObj;
+}
+
+deleteFromEmployeeByKey(employee, streetAddress);
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+  delete object[key];
+  return object;
+}
